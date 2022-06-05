0e3c13713bed3a21758a987ecabbcdaab088da0f
Egor147 <egor8313@yandex.ru>
2022-06-05 14:03:23 +0300

    Исправленные (Но не до конца) Желе

--- Assets/Scripts 1/Jelly.cs
+++ Assets/Scripts 1/Jelly.cs
@@ -8,7 +8,8 @@ public class Jelly : MonoBehaviour
 
    void OnTriggerEnter(Collider other){
       if (other.gameObject.CompareTag("Player")){
-         other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0,Ratio,0), ForceMode.Impulse);
+         var rb = other.gameObject.GetComponent<Rigidbody>();
+        rb.velocity =  new Vector3(rb.velocity.x, Ratio, rb.velocity.z);//AddForce(new Vector3(0,Ratio,0), ForceMode.Impulse);
          PlayerController.Jumping = true;
       }
    }
