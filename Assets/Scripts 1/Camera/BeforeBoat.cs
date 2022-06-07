5d2bd41ec89f27305a2640f88af8dfa5157c5699
Egor147 <egor8313@yandex.ru>
2022-06-07 03:27:10 +0300

    Анимации + новые чекпоинты и изменение позиции камеры

--- Assets/Scripts 1/Camera/BeforeBoat.cs
+++ Assets/Scripts 1/Camera/BeforeBoat.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class BeforeBoat : MonoBehaviour
 {
     [SerializeField] private Vector3 NewCameraRotation;
+
     [SerializeField] private Vector3 NewOffset;
-    [SerializeField] private GameObject camera;
+
     void OnTriggerEnter(Collider other){
+
         if (other.gameObject.CompareTag("Player")){
+
             CameraMovement.Rotat = NewCameraRotation;
+
             CameraMovement.Offset = NewOffset;
+
+
         }
+
+
     }
+
+
+
 }
