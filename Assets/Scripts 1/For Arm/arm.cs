4fafca268ab5688ab94eec2a6673243571b8242d
Egoooooooor <egogor.and@yandex.ru>
2022-06-08 20:07:22 +0300

    Dolgno rabotat

---
+++ Assets/Scripts 1/For Arm/arm.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arm : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] private float Speed;
+    [SerializeField] private bool caught = false;
+    private Vector3 target;
+    
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        if (!caught)
+            rb.velocity = new Vector3(Speed, rb.velocity.y, rb.velocity.z);
+        else{
+            transform.position = Vector3.MoveTowards(transform.position, target, Mathf.Abs(Speed) * 10 * Time.deltaTime);
+            PlayerController.GameOver = true;
+            //transform.position += (target.transform.position - transform.position).normalized * Mathf.Abs(Speed) * Time.deltaTime;
+        }
+    }
+
+
+    /*void OnTriggerStay(Collider other){
+        if (other.gameObject.CompareTag("Player")){
+            transform.position = Vector3.MoveTowards(transform.position, other.gameObject.transform.position, Speed * 10 * Time.deltaTime);
+        }
+    }*/
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Player")){
+            rb.velocity = new Vector3(0,0,0);
+            target = new Vector3(other.gameObject.transform.position.x + 7.489729f,  other.gameObject.transform.position.y + 1,  other.gameObject.transform.position.z);
+            caught = true;
+        }
+    }
+
+}
--- Assets/Scripts 1/Player/PlayerController.cs
+++ Assets/Scripts 1/Player/PlayerController.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float JumpPower, PushXSpeed, PushZSpeed, RotationAngle, BoostInSlipper, RunJumpPowerRatio;
+    [SerializeField] private float Jump_height, RotationAngle, BoostInSlipper;
     public float Speed;
     [SerializeField] private GameObject DeadMenu;
     private float timer = 0, SSpeed;
     [SerializeField] private Vector3 ScaleWhenSlide;
-    public static bool GameOver, Jumping, Pushing;
-    private bool Running, NormalScale, Slipper;
+    public static bool GameOver, Jumping, Slipper;
+    private bool NormalScale;
     private Vector3 StartScale, Rotate;
     Transform tr;
     Rigidbody rb;
     public static float k = 0f;
+    [SerializeField] private Animator Anim;
+    private int _state;
+
+    public static float JumpPower;
     void Start()
     {
+        JumpPower = Jump_height;
         GameOver = false;
         Slipper = false;
-        Pushing = false;
         Jumping = false;
-        Running = false;
         NormalScale = true;
         tr = gameObject.GetComponent<Transform>();
         rb = gameObject.GetComponent<Rigidbody>();
@@ -35,30 +38,38 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         if (!GameOver){
-                if (Input.GetButton("Run")){
-                    Move(Speed*2);
-                    Running = true;
-                }
-                else{
-                    Running = false;
-                    Move(Speed);
-                }
-                if (NormalScale)
-                    if (Input.GetButtonDown("Slide"))
-                        Slide();
-        } else {
+            if (Input.GetButton("Horizontal") || Input.GetButton("Vertical") || Slipper){
+                if (!Jumping)
+                    if (!Slipper)
+                        _state = 1;
+                    else 
+                        _state = 3;
+                Move(Speed);
+            } else if (!Jumping) 
+                    _state = 0;
+            if (Input.GetButtonDown("Slide") && NormalScale)
+                Slide();
+            //Anim.SetInteger("state",_state);
+        } else 
             Dead();
-        }
     }
 
     void Update(){
-        if (!GameOver)
-            if (!Jumping && !Pushing)
+        if (!GameOver){
+            if (!Jumping)
                 if (Input.GetButtonDown("Jump")){
-                    if (Running)
-                        Jump(JumpPower+k+RunJumpPowerRatio);
-                    else Jump(JumpPower);
+                    Jump(JumpPower);
+                    _state = 2;
                 }
+                else{
+                    if (_state == 4)
+                        _state = 5;
+                }
+            else{
+                _state = 4;
+            }
+            Anim.SetInteger("state",_state);
+        }
     }
 
     public void Move(float Speed){
@@ -69,7 +80,7 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector3(inputX*Speed,rb.velocity.y,inputZ*-Speed);
         }else{
             timer += Time.deltaTime;
-            rb.velocity = new Vector3(SSpeed*inputX + BoostInSlipper*timer,rb.velocity.y,inputZ*Speed);
+            rb.velocity = new Vector3(Speed*inputX + BoostInSlipper*timer,rb.velocity.y,inputZ*-Speed);
         }
     
         if (inputX<0)
@@ -93,26 +104,18 @@ public class PlayerController : MonoBehaviour
 
     void Jump( float HeightJump){
         Jumping = true;
+        _state = 4;
         rb.velocity = new Vector3(rb.velocity.x, HeightJump, rb.velocity.z);
     }
 
     void OnTriggerEnter(Collider other){
-        if(other.gameObject.CompareTag("Ground")){
+        //if(other.gameObject.CompareTag("Ground")){
+        if (Jumping && !other.gameObject.CompareTag("Jelly")){
             Jumping = false;
             k=0;
         }
-
-        else if (other.gameObject.CompareTag("Slippery")){
-            Slipper = true;
-            SSpeed = rb.velocity.x;
-        }
-    }
-    
-    void OnTriggerExit(Collider other){
-        if (other.gameObject.CompareTag("Slippery")){
-            Slipper = false;
-            timer = 0;
-        }
+           // _state = 5;
+        //}
     }
     void Rotation (float Rotation){
         tr.rotation = Quaternion.AngleAxis(Rotation, Vector3.up);
